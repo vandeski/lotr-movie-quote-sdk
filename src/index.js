8e143cb8e078cd685d4eb8b775bf4f067c2ed5f7
Michael J Vadney <mjvadney@gmail.com>
2023-06-01 16:46:23 -0500

    Initial connections to api created

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
+import axios from "axios";
+
 export default class lotrMovieQuoteApi {
-    constructor(accessToken) {
-        // Consider having this set when the use intializes this package
-        // Or is just might be set by default as this token for this package
-        this.ACCESS_TOKEN = "zzgm96PieoWg-r49eW-i"
-        // Consider making "v2" a constant that can be easily changed later as needed
-        this.BASE_URL = "https://the-one-api.dev/v2"
-        this.MOVIE_BASE_URL = `${this.BASE_URL}/movie`
-        this.QUOTE_BASE_URL = `${this.BASE_URL}/quote`
-    }
+  constructor(accessToken) {
+    this.ACCESS_TOKEN = accessToken;
+    // Consider making "v2" a constant that can be easily changed later as needed
+    this.BASE_URL = "https://the-one-api.dev/v2";
+    this.MOVIE_BASE_URL = `/movie`;
+    this.QUOTE_BASE_URL = `/quote`;
 
-    // List of all movies, including the "The Lord of the Rings" and the "The Hobbit" trilogies
-    getAllMovies() {
-
-    }
-
-    // Request one specific movie by id
-    getMovie(id) {
-
-    }
-
-    // Request all movie quotes for one specific movie by id (only working for the LotR trilogy)
-    getAllQuotesByMovie(id) {
-
-    }
-
-    // List of all movie quotes
-    getAllQuotes() {
-
-    }
-
-    // Request one specific movie quote by id
-    getQuote(id) {
-
-    }
+    this.AxiosInstance = axios.create({
+      baseURL: this.BASE_URL,
+      timeout: 1000,
+      headers: { Authorization: `Bearer ${this.ACCESS_TOKEN}` },
+    });
   }
+
+  // List of all movies, including the "The Lord of the Rings" and the "The Hobbit" trilogies
+  async getAllMovies() {
+    let resp = await this.AxiosInstance.get(this.MOVIE_BASE_URL);
+    return resp.data.docs;
+  }
+
+  // Request one specific movie by id
+  async getMovie(id) {
+    let resp = await this.AxiosInstance.get(`${this.MOVIE_BASE_URL}/${id}`);
+    return resp.data.docs;
+  }
+
+  // Request all movie quotes for one specific movie by id (only working for the LotR trilogy)
+  async getAllQuotesByMovie(id) {
+    let resp = await this.AxiosInstance.get(
+      `${this.MOVIE_BASE_URL}/${id}/${this.QUOTE_BASE_URL}`
+    );
+    return resp.data.docs;
+  }
+
+  // List of all movie quotes
+  async getAllQuotes() {
+    let resp = await this.AxiosInstance.get(`${this.QUOTE_BASE_URL}`);
+    return resp.data.docs;
+  }
+
+  // Request one specific movie quote by id
+  async getQuote(id) {
+    let resp = await this.AxiosInstance.get(`${this.QUOTE_BASE_URL}/${id}`);
+    return resp.data.docs;
+  }
+}
